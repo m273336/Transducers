9853d6e4e6e4b1e5161b3b0d378a23b69246c937
m282082 <m282082@usna.edu>
2025-12-07 18:46:59 -0500

    Coolio

---
+++ doNothing.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    frgui = open("fromGUI","r")
+    togui = open("toGUI","w")
+    count = 0
+    while True:
+        # Read event message
+        try:
+            msg,data = frgui.readline().split(maxsplit=1);
+        except:
+            break
+        count += 1
+        print(f'Read({count}): {msg} {data}',file=sys.stderr) #4DEBUG!
+
+        # Choose action message to respond with
+        # IMPORTANT! My example code does something silly ...
+        # - if you click on a vertex, the "Recentering Pane" will appear,
+        #   and when you leave the "canvas", it disappears.  See why?
+        response = "noop 0"
+        match msg:
+            case 'mouseDownVertex':
+                response = "showCP " + data
+            case 'mouseLeaveCanvas':
+                response = "hideCP " + data
+            case _:
+                print(f'No handler for: {msg}',file=sys.stderr)
+        
+        # Respond to GUI - flush to send line immediately!
+        print(response + "\n",file=togui,flush=True)
+        print(f'Sent({count}): {response}',file=sys.stderr) #4DEBUG!
