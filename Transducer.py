d9cdecdb7c52629760d705ce9a11b5b5b70f6f2a
m282082 <m282082@usna.edu>
2025-12-07 21:53:12 -0500

    Upgraded Transducer to pass args if in right format

--- Transducer.py
+++ Transducer.py
@@ -7,11 +7,19 @@ class Transducer:
 
     # input: a string into the transducer
     # output: the output string given the current state and transition function
-    #         return "noop 0" if the key is not in the map
+    # Handles data input through args if action message needing data is at end of transition
+    # return "noop 0" if the key is not in the map
     def step(self, w):
         try:
-            q,y = self.Tmap[(self.s,w)]
-            self.s = q
-            return y
+            if(w[-1] == '0'):
+                q,y = self.Tmap[(self.s,w)]
+                self.s = q
+                return y
+            else:
+                i = w.index(" ")
+                q,y = self.Tmap[(self.s,w[:i])]
+                self.s = q
+                return y + " " + w[i:]
+            
         except KeyError:
             return "noop 0"
