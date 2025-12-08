1fe23ea8934655451d2faf5a973d3301ca787668
m273336 <m273336@usna.edu>
2025-12-07 19:18:17 -0500

    doNothing.py

--- doNothing.py
+++ doNothing.py
@@ -5,6 +5,14 @@ if __name__ == "__main__":
     frgui = open("fromGUI","r")
     togui = open("toGUI","w")
     count = 0
+
+    Q = {0, 1, 2, 3, 4, 5}
+    inputAlphabet = {"click", "clockOnCanvas", }
+    outputAlphabet = {}
+    transitions = {}
+    S = 1
+    transducer = Transducer(Q, inputAlphabet, outputAlphabet, transitions, S)
+     
     while True:
         # Read event message
         try:
@@ -18,15 +26,20 @@ if __name__ == "__main__":
         # IMPORTANT! My example code does something silly ...
         # - if you click on a vertex, the "Recentering Pane" will appear,
         #   and when you leave the "canvas", it disappears.  See why?
+
+        
         response = "noop 0"
-        match msg:
-            case 'mouseDownVertex':
-                response = "showCP " + data
-            case 'mouseLeaveCanvas':
-                response = "hideCP " + data
-            case _:
-                print(f'No handler for: {msg}',file=sys.stderr)
+        response = transducer.step(msg)
+        #match msg:
+            #case 'mouseDownVertex':
+                #response = "showCP " + data
+            #case 'mouseLeaveCanvas':
+                #response = "hideCP " + data
+            #case _:
+                #print(f'No handler for: {msg}',file=sys.stderr)
+            
         
         # Respond to GUI - flush to send line immediately!
         print(response + "\n",file=togui,flush=True)
         print(f'Sent({count}): {response}',file=sys.stderr) #4DEBUG!
+
